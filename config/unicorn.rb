a3f7dcabf098ea249f0bef792f1c1bba05650020
Erik <edawebdesign@gmail.com>
2014-02-23 17:03:50 -0500

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,8 +8,5 @@ listen "/tmp/unicorn.convertbarcode.sock"
 worker_processes 2
 timeout 30
 
-# Force the bundler gemfile environment variable to
-# reference the capistrano "current" symlink
-before_exec do |_|
-  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+
 end
